4ac0da9ff6c7d02e77bdf02107b9b2f3e6113eb6
AniXq <anirban9798@gmail.com>
2024-08-26 15:16:58 +0530

    Update mongo.py
    Signed-off-by: AniXq <anirban9798@gmail.com>

--- AnonXMusic/utils/mongo.py
+++ AnonXMusic/utils/mongo.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-from FallenRobot import MONGO_DB_URI
+from AnonXMusic import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
 db = mongo.FallenRobot
